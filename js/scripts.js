4f438c1a1b152a05de7ab8a84cbd2db471c4985c
Tony Froccaro <tfroccaro@gmail.com>
2022-01-23 16:55:45 -0500

    Flashcards html, css and js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $("#javascript").click(function () {
+    $("#javascript button").toggle();
+    $("#javascript p").toggle();
+  });
+  $("#operators").click(function () {
+    $("#operators button").toggle();
+    $("#operators p").toggle();
+  });
+  $("#variables").click(function () {
+    $("#variables button").toggle();
+    $("#variables p").toggle();
+  });
+  $("#functions").click(function () {
+    $("#functions button").toggle();
+    $("#functions p").toggle();
+  });
+  $("#methods").click(function () {
+    $("#methods button").toggle();
+    $("#methods p").toggle();
+  });
+  $("#arguments").click(function () {
+    $("#arguments button").toggle();
+    $("#arguments p").toggle();
+  });
+});
